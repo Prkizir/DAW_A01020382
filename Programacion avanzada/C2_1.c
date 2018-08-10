bb54c8ebfa7ace8856213d8cf1b790e67a68e872
Prkizir <35320275+Prkizir@users.noreply.github.com>
2018-08-10 14:57:43 -0500

    Another Test

---
+++ Programacion avanzada/C2_1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef unsigned long long ull;
+
+struct x {
+} X;
+
+unsigned long long z;
+ull p;
+
+struct x myVar;
+X otherVar;
+
+
+union Y{
+};
+
+union Y myOtherVar;
---
+++ Programacion avanzada/C2_2.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+void swap(int a*, int b*){
+	int aux;
+	aux = a*;
+	*a = *b;
+	*b = aux;
+}
+
+int mayor(int a, int b){
+	return (a > b);
+}
+
+int menor(int a, int b){
+	return (a < b);
+}
+
+int aleatorio(int a, int b){
+	if(rand() % 2){
+		return(a < b);
+	}else{
+		return (a > b);
+	}
+}
+
+void bsort(int *arr, int n, int (*func) (int, int)){
+	int i, j;
+
+	for(i = n - 1; i > 0; i--){
+		for(j = 0; j < i; j++){
+			if(fn(arr[j], arr[j+1])){
+				swap((arr + j), (arr + j + 1));
+			}
+		}
+	}
+}
+
+void display(int *arr, int n){
+	int i;
+
+	for(i = 0; i < n; i++){
+		printf("%i", arr[i]);
+	}
+
+	printf(¨\n\n¨);
+}
+
+int main(int argc, char* argv[]){
+	int a[10] = {10,9,8,7,6,5,4,3,2,1};
+
+	srand(time(0));
+
+	display(a,10);
+	
+	bsort(a, 10, mayor);
+	display(a,10);
+
+	bsort(a, 10, menor);
+	display(a,10);
+
+	bsort(a, 10, aleatorio);
+	display(a,10);
+
+	return 0;
+}
