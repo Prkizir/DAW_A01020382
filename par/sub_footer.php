133a4762016527f3731b33eb69d5bc9ffa21de48
Prkizir <A01020382@itesm.mx>
2018-10-01 21:37:42 -0500

    Renew Repo

---
+++ par/sub_footer.php
@@ -0,0 +1,15 @@
+        <footer>
+            <p>Author: Sergio Isaac Mercado Silvano A01020382<br>
+                Hecho con:<br>
+                <a href="http://brackets.io/">  
+                    <img id="brkicon" src="../img/bracketslogo.png">
+                </a>
+            </p>
+        </footer>
+        
+        <script src="../js/vendor/jquery.js"></script>
+        <script src="../js/vendor/what-input.js"></script>
+        <script src="../js/vendor/foundation.js"></script>
+        <script src="../js/app.js"></script>
+    </body>
+</html>
--- par/sub_header.php
+++ par/sub_header.php
@@ -1,14 +1,15 @@
 <!doctype html>
 <html class="no-js" lang="en" dir = "ltr">
     <head>
-        <title> A01020382 </title>
-        <link rel="stylesheet" href="css/foundation.css">
-        <link rel="stylesheet" href="css/app.css">
+        <title> Lab 1 </title>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="../css/foundation.css">
+        <link rel="stylesheet" href="../css/app.css">
     </head>
     
-    <body class="indexBody">
+    <body class="lab">
         <div class="header">
-            <h1>DAW</h1>
+            <h1>Lab 1</h1>
             <div class="top-bar">
                 <div class="top-bar-left">
                     <ul class="dropdown menu" data-dropdown-menu>
@@ -16,9 +17,9 @@
                         <li>
                             <a href="#">Labs</a>
                             <ul class="menu vertical">
-                                <li><a href="./labs/Lab1.html">Lab 1</a></li>
-                                <li><a href="./labs/Lab3.html">Lab 3</a></li>
-                                <li><a href="./labs/Lab4.html">Lab 4</a></li>
+                                <li><a href="../labs/Lab1.html">Lab 1</a></li>
+                                <li><a href="../labs/Lab3.html">Lab 3</a></li>
+                                <li><a href="../labs/Lab4.html">Lab 4</a></li>
                                 <li><a href="#">Lab 5</a></li>
                                 <li><a href="#">Lab 6</a></li>
                                 <li><a href="#">Lab 7</a></li>
@@ -27,14 +28,14 @@
                             </ul>
                         </li>
                         <li><a href="#">About Me</a></li>
+                        
+                        <div class="top-bar-right">
+                            <li><a href="../index.html">Back to Main</a></li>
+                        </div>
                     </ul>
                 </div>
             </div>
         </div>
-
-        <script src="js/vendor/jquery.js"></script>
-        <script src="js/vendor/what-input.js"></script>
-        <script src="js/vendor/foundation.js"></script>
-        <script src="js/app.js"></script>
-    </body>
-</html>
+        
+        
+        <h2>Cuestionario</h2>
